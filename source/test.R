079be02af23c78fdbf0d0b1e439778085200534e
yoojanghun <yoojanghun@naver.com>
2025-09-24 22:19:18 +0900

    함수 예시

---
+++ source/test.R
@@ -0,0 +1,9 @@
+x <- c(1, 2, 3)   # combine 함수 여러 값을 하나의 벡터로 묶어줌
+
+class(x)          # class 함수 data type 확인 함수 (typeof() 함수도 있음)
+
+mean(x)           # 평균 계산 함수
+
+length(x)         # 변수의 원소 개수 
+
+head(x)
